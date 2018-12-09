f097d74d6407976516b984850702a08037370daf
Kaushik <45584338+opensourcey@users.noreply.github.com>
2018-12-09 16:33:30 +0530

    Create main_activity.java

---
+++ main_activity.java
@@ -0,0 +1,8 @@
+private ConstraintLayout layout;
+
+layout = findViewById(R.id.layout);
+
+        AnimationDrawable animationDrawable = (AnimationDrawable) layout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(3000);
+        animationDrawable.start();
